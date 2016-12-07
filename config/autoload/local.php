0c4c7784f131f0933c429e184ca260f50c943721
Rolf Bansbach <rolfisub@gmail.com>
2016-12-06 22:30:27 -0500

    fix for local config

---
+++ config/autoload/local.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'db' => [
+        'adapters' => [
+            'db1' => [
+                'database' => 'riskman',
+                'driver' => \Mysqli::class,
+                'hostname' => '192.168.1.53',
+                'username' => 'riskman',
+                'password' => 'lXytl3kAvDCXMzFg',
+                'port' => '3306', 
+            ],
+        ],
+    ],
+];
