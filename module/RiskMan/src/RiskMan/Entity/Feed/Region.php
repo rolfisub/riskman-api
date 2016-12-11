fdd6023271fe1a9edffe3ee96f268bd82aea0c86
Rolf Bansbach <rolfisub@gmail.com>
2016-12-10 21:49:20 -0500

    doctrine 2 more entities and domain logic

--- module/RiskMan/src/RiskMan/Entity/Feed/Region.php
+++ module/RiskMan/src/RiskMan/Entity/Feed/Region.php
@@ -40,5 +40,134 @@ class Region extends BaseFeedEntity
      * @ORM\Column(type="string", length=64)
      */
     protected $name;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Event", mappedBy="Region")
+     */
+    protected $events;
+    
+    /**
+     * Initialize
+     */
+    public function __construct() 
+    {
+       $this->events = new ArrayCollection();
+    }
 
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set regionId
+     *
+     * @param string $regionId
+     *
+     * @return Region
+     */
+    public function setRegionId($regionId)
+    {
+        $this->region_id = $regionId;
+
+        return $this;
+    }
+
+    /**
+     * Get regionId
+     *
+     * @return string
+     */
+    public function getRegionId()
+    {
+        return $this->region_id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Region
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set bookId
+     *
+     * @param integer $bookId
+     *
+     * @return Region
+     */
+    public function setBookId($bookId)
+    {
+        $this->book_id = $bookId;
+
+        return $this;
+    }
+
+    /**
+     * Get bookId
+     *
+     * @return integer
+     */
+    public function getBookId()
+    {
+        return $this->book_id;
+    }
+
+    /**
+     * Add event
+     *
+     * @param \RiskMan\Entity\Feed\Event $event
+     *
+     * @return Region
+     */
+    public function addEvent(\RiskMan\Entity\Feed\Event $event)
+    {
+        $this->events[] = $event;
+
+        return $this;
+    }
+
+    /**
+     * Remove event
+     *
+     * @param \RiskMan\Entity\Feed\Event $event
+     */
+    public function removeEvent(\RiskMan\Entity\Feed\Event $event)
+    {
+        $this->events->removeElement($event);
+    }
+
+    /**
+     * Get events
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getEvents()
+    {
+        return $this->events;
+    }
 }
