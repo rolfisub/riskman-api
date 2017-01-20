88dd5aebf6f3e20c8fb8cbf9cb87c64149bf917c
Rolf Bansbach <rolfisub@gmail.com>
2017-01-19 23:31:52 -0500

    added odd and odd selections

---
+++ module/RiskMan/src/RiskMan/Model/Feed/OddSelection.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace RiskMan\Model\Feed;
+use RiskMan\Model\ModelObject;
+
+
+/**
+ * Description of Event
+ *
+ * @author rolf
+ */
+class OddSelection extends ModelObject
+{
+    
+
+}
