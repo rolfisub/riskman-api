fdf77fee728c83899a744c914a01c9cefaf3b884
Rolf Bansbach <rolf@softecdigital.com>
2017-02-27 21:50:32 -0500

    Adding Admin module

---
+++ module/Admin/config/module.config.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @author Rolf Bansbach
+ */
+
+return array(
+    'router' => array(
+        'routes' => array(
+            'Admin' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route'    => '/Admin',
+                    'defaults' => array(
+                        'controller' => 'Admin\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'service_manager' => array(
+        'abstract_factories' => array(
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Db\Adapter\AdapterAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory',
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Admin\Controller\Index' => 'Admin\Controller\IndexController',
+        ),
+    ),
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+);
---
+++ module/Admin/src/Admin/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace Admin\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        
+        return new ViewModel();
+    }
+}
