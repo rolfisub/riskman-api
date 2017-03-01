5d7bbf918e6d234df71dac3bc7267dd2b4c4bb26
Rolf Bansbach <rolf@softecdigital.com>
2017-02-28 20:38:38 -0500

    Set up frontend dependencies (Admin)

--- public/app/js/require.config.js
+++ public/app/js/require.config.js
@@ -5,3 +5,49 @@
  */
 
 
+requirejs.config({
+    baseUrl: '../../app/js',
+    
+    paths:{
+        'jquery':'lib/jquery-1.10.2',
+        'bootstrap':'lib/bootstrap.min',
+        'chartist':'lib/chartist.min',
+        'bootstrap-notify':'lib/bootstrap-notify',
+        'googlemaps':'https://maps.googleapis.com/maps/api/js?sensor=false',
+        'light-bootstrap-dashboard':'lib/light-bootstrap-dashboard',
+    },
+    
+    
+    shim:{
+        'jquery':{
+           exports:'$'  
+        },
+        'bootstrap':{
+            deps:['jquery']
+        },
+        'chartist':{
+            deps:['jquery', 'bootstrap']
+        },
+        'bootstrap-notify':{
+            deps:['jquery', 'bootstrap']
+        },
+        'light-bootstrap-dashboard':{
+            deps:['jquery', 'bootstrap', 'chartist', 'bootstrap-notify']
+        }
+        
+        
+    }
+});
+
+require([
+    'jquery',
+    'bootstrap',
+    'chartist',
+    'bootstrap-notify',
+    'light-bootstrap-dashboard'
+], function($){
+    //do nothing
+   
+});
+
+
