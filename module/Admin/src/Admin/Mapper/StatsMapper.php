37d1e1d1ffcc3ce1677c7471643c61c349e7f66b
Rolf Bansbach <rolf@softecdigital.com>
2017-03-14 02:19:36 -0400

    getting some data for stats

--- module/Admin/src/Admin/Mapper/StatsMapper.php
+++ module/Admin/src/Admin/Mapper/StatsMapper.php
@@ -8,22 +8,115 @@
 
 namespace Admin\Mapper;
 
-use Zend\Db\Adapter\Adapter;
+use Admin\Mapper\AbstractMapper;
+
+use Zend\Db\Sql\Where;
+use Zend\Db\Sql\Expression as SqlExpression;
+
 /**
  * Description of StatsMapper
  *
  * @author rolf
  */
-class StatsMapper 
+class StatsMapper extends AbstractMapper
 {
-    protected $adapter;
-    public function __construct(Adapter $adapter) 
-    {
-        $this->adapter = $adapter;
-    }
     
     public function testMeMapper()
     {
         return [];
+    }
+    
+    public function getApiGeneralStats()
+    {
+        
+        return [
+            'api-requests' => $this->getApiRequestsCount(),
+            'api-errors' => $this->getApiErrorsCount(),
+            'server-exceptions' => $this->getApiServerErrorsCount(),
+            'bets' => $this->getApiBetsCount(),
+            'suggestions' => 0,
+            'admins' => 1,
+            'users' => 1,
+            'books' => 2
+        ];
+    }
+    
+    /**
+     * gets the amount of successful requests
+     * @return int
+     */
+    private function getApiRequestsCount()
+    {
+        $s = $this->sql->select('apilog');
+        $s->columns([
+            'apilogCount' => new SqlExpression('COUNT(*)')
+        ]);
+        $w = new Where();
+        $w->equalTo('http_code', 200);
+        $s->where($w);
+        $result = $this->queryObject($s);
+        $arr = $result->toArray();
+        return $arr[0]['apilogCount'];
+    }
+    
+    /**
+     * gets the amount of api error requests
+     * @return int
+     */
+    private function getApiErrorsCount()
+    {
+        $s = $this->sql->select('apilog');
+        $s->columns([
+            'apilogCount' => new SqlExpression('COUNT(*)')
+        ]);
+        $w = new Where();
+        $w->greaterThanOrEqualTo('http_code', 400)
+            ->AND
+            ->lessThan('http_code', 500);
+        $s->where($w);
+        $result = $this->queryObject($s);
+        $arr = $result->toArray();
+        return $arr[0]['apilogCount'];
+    }
+    
+    /**
+     * gets the amount of server error requests
+     * @return int
+     */
+    private function getApiServerErrorsCount()
+    {
+        $s = $this->sql->select('apilog');
+        $s->columns([
+            'apilogCount' => new SqlExpression('COUNT(*)')
+        ]);
+        $w = new Where();
+        $w->equalTo('http_code', 500);
+        $s->where($w);
+        $result = $this->queryObject($s);
+        $arr = $result->toArray();
+        return $arr[0]['apilogCount'];
+    }
+    
+    /**
+     * gets the amount of bets (single + multiple)
+     * @return int
+     */
+    private function getApiBetsCount()
+    {
+        $singlesCount = $this->getBetsCount('single');
+        $multipleCount = $this->getBetsCount('multiple');
+        $total = $singlesCount + $multipleCount;
+        return $total;
+    }
+    
+    private function getBetsCount($table)
+    {
+        $s = $this->sql->select($table);
+        $s->columns([
+            'count' => new SqlExpression('COUNT(*)')
+        ]);
+        $result = $this->queryObject($s);
+        $arr = $result->toArray();
+        return $arr[0]['count'];
     }
 }
