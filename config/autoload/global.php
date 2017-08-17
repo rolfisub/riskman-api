3006eb17d4f9602a864eafe5b5f180d78ee22495
Rolf Bansbach <rolf@softecdigital.com>
2017-08-17 00:27:39 -0400

    RIS-11 db and foundation oauth2

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -5,4 +5,22 @@ return array(
             'db1' => array(),
         ),
     ),
+    'router' => array(
+        'routes' => array(
+            'oauth' => array(
+                'options' => array(
+                    'spec' => '%oauth%',
+                    'regex' => '(?P<oauth>(/oauth))',
+                ),
+                'type' => 'regex',
+            ),
+        ),
+    ),
+    'zf-mvc-auth' => array(
+        'authentication' => array(
+            'map' => array(
+                'RiskMan\\V1' => 'oauth2riskman',
+            ),
+        ),
+    ),
 );
