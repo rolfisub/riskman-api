080e01c2b6c2eb2d50a7d551eb2b95f8506b9fdf
Rolf Bansbach <rolf@softecdigital.com>
2017-08-24 02:48:05 -0400

    RIS-23 subtask RIS-111 update db schema

--- module/RiskMan/DatabaseSchema/riskman.sql
+++ module/RiskMan/DatabaseSchema/riskman.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 28, 2017 at 06:59 PM
--- Server version: 5.7.16-0ubuntu0.16.04.1
--- PHP Version: 7.0.8-0ubuntu0.16.04.3
+-- Generation Time: Aug 24, 2017 at 02:46 AM
+-- Server version: 5.7.19-0ubuntu0.16.04.1
+-- PHP Version: 7.0.22-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -25,11 +25,40 @@ USE `riskman`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admins`
+--
+
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(100) NOT NULL,
+  `pass_word` varchar(100) NOT NULL,
+  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_name` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins_info`
+--
+
+CREATE TABLE IF NOT EXISTS `admins_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(64) NOT NULL,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `apilog`
 --
 
-CREATE TABLE `apilog` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `apilog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `ip_address` varchar(32) NOT NULL,
   `service_name` text NOT NULL,
@@ -40,19 +69,25 @@ CREATE TABLE `apilog` (
   `http_message` text NOT NULL,
   `received_data` text NOT NULL,
   `sent_data` text NOT NULL,
-  `datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `datetime` (`datetime`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=153930 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `book`
+-- Table structure for table `books`
 --
 
-CREATE TABLE `book` (
-  `id` int(11) NOT NULL,
-  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE IF NOT EXISTS `books` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -60,16 +95,17 @@ CREATE TABLE `book` (
 -- Table structure for table `event`
 --
 
-CREATE TABLE `event` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `event` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `event_id` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
   `datetime` datetime DEFAULT NULL,
   `sport_id` int(11) DEFAULT NULL,
   `league_id` int(11) DEFAULT NULL,
   `region_id` int(11) DEFAULT NULL,
-  `book_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `book_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9430 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -77,12 +113,13 @@ CREATE TABLE `event` (
 -- Table structure for table `league`
 --
 
-CREATE TABLE `league` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `league` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `league_id` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
-  `book_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `book_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1117 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -90,13 +127,17 @@ CREATE TABLE `league` (
 -- Table structure for table `multiple`
 --
 
-CREATE TABLE `multiple` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `multiple` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `multiple_id` varchar(32) NOT NULL,
+  `player_id` int(11) DEFAULT NULL,
   `risk` float NOT NULL,
   `win` float NOT NULL,
-  `book_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `book_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `multiple_id` (`multiple_id`),
+  KEY `book_id` (`book_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -104,15 +145,126 @@ CREATE TABLE `multiple` (
 -- Table structure for table `multipleselection`
 --
 
-CREATE TABLE `multipleselection` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `multipleselection` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `multiple_selection_id` varchar(32) NOT NULL,
+  `multiple_id` int(11) NOT NULL,
   `event_id` int(11) NOT NULL,
   `odd_id` int(11) NOT NULL,
   `odd_selection_id` int(11) NOT NULL,
   `odd` float NOT NULL,
   `points` float NOT NULL,
-  `book_id` int(11) NOT NULL
+  `book_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `odd_id` (`odd_id`),
+  KEY `multiple_selection_id` (`multiple_selection_id`),
+  KEY `odd_selection_id` (`odd_selection_id`),
+  KEY `book_id` (`book_id`),
+  KEY `event_id` (`event_id`),
+  KEY `multiple_id` (`multiple_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_access_tokens`
+--
+
+CREATE TABLE IF NOT EXISTS `oauth_access_tokens` (
+  `access_token` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`access_token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_authorization_codes`
+--
+
+CREATE TABLE IF NOT EXISTS `oauth_authorization_codes` (
+  `authorization_code` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `redirect_uri` varchar(2000) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL,
+  `id_token` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`authorization_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_clients`
+--
+
+CREATE TABLE IF NOT EXISTS `oauth_clients` (
+  `client_id` varchar(80) NOT NULL,
+  `client_secret` varchar(80) NOT NULL,
+  `redirect_uri` varchar(2000) NOT NULL,
+  `grant_types` varchar(80) DEFAULT NULL,
+  `scope` varchar(2000) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_jwt`
+--
+
+CREATE TABLE IF NOT EXISTS `oauth_jwt` (
+  `client_id` varchar(80) NOT NULL,
+  `subject` varchar(80) DEFAULT NULL,
+  `public_key` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_refresh_tokens`
+--
+
+CREATE TABLE IF NOT EXISTS `oauth_refresh_tokens` (
+  `refresh_token` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`refresh_token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_scopes`
+--
+
+CREATE TABLE IF NOT EXISTS `oauth_scopes` (
+  `type` varchar(255) NOT NULL DEFAULT 'supported',
+  `scope` varchar(2000) DEFAULT NULL,
+  `client_id` varchar(80) DEFAULT NULL,
+  `is_default` smallint(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_users`
+--
+
+CREATE TABLE IF NOT EXISTS `oauth_users` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(2000) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -121,14 +273,18 @@ CREATE TABLE `multipleselection` (
 -- Table structure for table `odd`
 --
 
-CREATE TABLE `odd` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `odd` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `odd_id` varchar(32) NOT NULL,
   `name` varchar(128) DEFAULT NULL,
   `event_id` int(11) NOT NULL,
   `book_id` int(11) NOT NULL,
-  `datetime` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `odd_id` (`odd_id`),
+  KEY `event_id` (`event_id`),
+  KEY `book_id` (`book_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=142594 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -136,15 +292,35 @@ CREATE TABLE `odd` (
 -- Table structure for table `oddselection`
 --
 
-CREATE TABLE `oddselection` (
-  `id` int(11) NOT NULL,
-  `odd_selection_id` varchar(32) NOT NULL,
+CREATE TABLE IF NOT EXISTS `oddselection` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `odd_selection_id` varchar(64) NOT NULL,
   `name` varchar(128) DEFAULT NULL,
   `odd_id` int(11) NOT NULL,
-  `odd` double NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `odd` double DEFAULT NULL,
   `points` double DEFAULT NULL,
-  `book_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `book_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `odd_selection_id` (`odd_selection_id`),
+  KEY `odd_id` (`odd_id`),
+  KEY `book_id` (`book_id`),
+  KEY `event_id` (`event_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=420921 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player`
+--
+
+CREATE TABLE IF NOT EXISTS `player` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `player_id` varchar(32) NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `book_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -152,12 +328,13 @@ CREATE TABLE `oddselection` (
 -- Table structure for table `region`
 --
 
-CREATE TABLE `region` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `region` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `region_id` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
-  `book_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `book_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -165,18 +342,25 @@ CREATE TABLE `region` (
 -- Table structure for table `single`
 --
 
-CREATE TABLE `single` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `single` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `single_id` varchar(32) NOT NULL,
   `event_id` int(11) NOT NULL,
   `odd_id` int(11) NOT NULL,
   `odd_selection_id` int(11) NOT NULL,
+  `player_id` int(11) DEFAULT NULL,
   `risk` float NOT NULL,
   `win` float DEFAULT NULL,
   `odd` float NOT NULL,
   `points` float NOT NULL,
-  `book_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `book_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `single_id` (`single_id`),
+  KEY `odd_id` (`odd_id`),
+  KEY `odd_selection_id` (`odd_selection_id`),
+  KEY `book_id` (`book_id`),
+  KEY `event_id` (`event_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -184,160 +368,29 @@ CREATE TABLE `single` (
 -- Table structure for table `sport`
 --
 
-CREATE TABLE `sport` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `sport` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `sport_id` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
-  `book_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `book_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Table structure for table `users`
 --
 
---
--- Indexes for table `apilog`
---
-ALTER TABLE `apilog`
-  ADD PRIMARY KEY (`id`);
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(100) NOT NULL,
+  `pass_word` varchar(100) NOT NULL,
+  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_name` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
---
--- Indexes for table `book`
---
-ALTER TABLE `book`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `event`
---
-ALTER TABLE `event`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `league`
---
-ALTER TABLE `league`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `multiple`
---
-ALTER TABLE `multiple`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `multiple_id` (`multiple_id`),
-  ADD KEY `book_id` (`book_id`);
-
---
--- Indexes for table `multipleselection`
---
-ALTER TABLE `multipleselection`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `odd_id` (`odd_id`),
-  ADD KEY `multiple_selection_id` (`multiple_selection_id`),
-  ADD KEY `odd_selection_id` (`odd_selection_id`),
-  ADD KEY `book_id` (`book_id`),
-  ADD KEY `event_id` (`event_id`);
-
---
--- Indexes for table `odd`
---
-ALTER TABLE `odd`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `odd_id` (`odd_id`),
-  ADD KEY `event_id` (`event_id`),
-  ADD KEY `book_id` (`book_id`);
-
---
--- Indexes for table `oddselection`
---
-ALTER TABLE `oddselection`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `odd_selection_id` (`odd_selection_id`),
-  ADD KEY `odd_id` (`odd_id`),
-  ADD KEY `book_id` (`book_id`);
-
---
--- Indexes for table `region`
---
-ALTER TABLE `region`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `single`
---
-ALTER TABLE `single`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `single_id` (`single_id`),
-  ADD KEY `odd_id` (`odd_id`),
-  ADD KEY `odd_selection_id` (`odd_selection_id`),
-  ADD KEY `book_id` (`book_id`),
-  ADD KEY `event_id` (`event_id`);
-
---
--- Indexes for table `sport`
---
-ALTER TABLE `sport`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `apilog`
---
-ALTER TABLE `apilog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
---
--- AUTO_INCREMENT for table `book`
---
-ALTER TABLE `book`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `event`
---
-ALTER TABLE `event`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `league`
---
-ALTER TABLE `league`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `multiple`
---
-ALTER TABLE `multiple`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `multipleselection`
---
-ALTER TABLE `multipleselection`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `odd`
---
-ALTER TABLE `odd`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `oddselection`
---
-ALTER TABLE `oddselection`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `region`
---
-ALTER TABLE `region`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `single`
---
-ALTER TABLE `single`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `sport`
---
-ALTER TABLE `sport`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
